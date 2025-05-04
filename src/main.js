d68e06c8c9d9c0650268a66c34f8138a944142a2
solehudin11001 <solehudin11001@gmail.com>
2025-05-04 11:17:40 +0700

    feature: navbar

--- src/main.js
+++ src/main.js
@@ -1,1 +1,13 @@
 import "./style.css";
+
+// Navigation
+
+const buttonMenu = document.getElementById("button-menu");
+
+buttonMenu.addEventListener("click", () => {
+  const isCollapse = buttonMenu.getAttribute("aria-expanded") === "true";
+  const mainNav = document.getElementById("menu");
+
+  buttonMenu.setAttribute("aria-expanded", !isCollapse);
+  mainNav.classList.toggle("collapse");
+});
