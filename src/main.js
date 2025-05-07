20b6dc287b8ab42841f45aa489911a3f176f4b4a
solehudin11001 <solehudin11001@gmail.com>
2025-05-07 10:52:24 +0700

    layout: section - testimonial - call to action footer

--- src/main.js
+++ src/main.js
@@ -11,3 +11,31 @@ buttonMenu.addEventListener("click", () => {
   buttonMenu.setAttribute("aria-expanded", !isCollapse);
   mainNav.classList.toggle("collapse");
 });
+
+// Slide
+
+const slides = document.querySelectorAll(".slide__item");
+const btnPrev = document.querySelector(".slide__prev");
+const btnNext = document.querySelector(".slide__next");
+
+let currentIndex = 0;
+
+// Helper function: Update slide visibility
+const updateSlide = () => {
+  slides.forEach((slide, index) =>
+    slide.classList.toggle("active", index === currentIndex)
+  );
+};
+
+// Go to specific slide
+const goToSlide = (index) => {
+  currentIndex = (index + slides.length) % slides.length;
+  updateSlide();
+};
+
+// Event listener
+btnPrev.addEventListener("click", () => goToSlide(currentIndex - 1));
+btnNext.addEventListener("click", () => goToSlide(currentIndex + 1));
+
+// Initialize
+updateSlide();
